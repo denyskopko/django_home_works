1746e499f95f8a58c9f6e0a7bdbfd43d51db1fd4
denyskopko <kopko.denis23@gmail.com>
2026-03-22 20:07:48 +0100

    add Metaclasses in all models, creted adminmodels :  TaskAdmin, SubTaskAdmin and CategoryAdmin

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from my_app.models import Task, SubTask, Category
 
-# Register your models here.
-admin.site.register(Task)
-admin.site.register(SubTask)
-admin.site.register(Category)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', "deadline", "status")
+    search_fields = ('title', 'created_at')
+    ordering = ('-created_at',)
+
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', "deadline", "status")
+    search_fields = ('title', 'created_at', "deadline")
+    ordering = ('-created_at',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_per_page = 5
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(SubTask, SubTaskAdmin)
+admin.site.register(Category, CategoryAdmin)
--- my_app/models.py
+++ my_app/models.py
@@ -7,21 +7,9 @@ STATUS_CHOICES = [
         ('In progress', 'In progress'),
         ('Pending', 'Pending'),
         ('Blocked', 'Blocked'),
-        ('Done', 'Done'),
-    ]
+        ('Done', 'Done'),]
 
-# Create your models here.
 class Task(models.Model):
-    """Модель Task:
-    Описание: Задача для выполнения.
-    Поля:
-    title: Название задачи. Уникально для даты.
-    description: Описание задачи.
-    categories: Категории задачи. Многие ко многим.
-    status: Статус задачи. Выбор из: New, In progress, Pending, Blocked, Done
-    deadline: Дата и время дедлайн
-    created_at: Дата и время создания. Автоматическое заполнение."""
-
     title : str = models.CharField(max_length=100, unique_for_date="created_at", verbose_name="task title")
     description:str  = models.TextField(help_text='Описание задачи', verbose_name="task description")
     categories : str  = models.ManyToManyField("Category", related_name='tasks', blank=True, verbose_name='task categories')
@@ -29,29 +17,47 @@ class Task(models.Model):
     deadline : datetime = models.DateTimeField(help_text="Конечная дата выполнения", verbose_name="task deadline")
     created_at : datetime = models.DateTimeField(auto_now_add=True, verbose_name="task created at")
 
+    class Meta:
+        db_table = "task_manager_task"
+        verbose_name = "Task"
+        ordering = ['-created_at']
+        constraints = [
+            models.UniqueConstraint(fields=['title'], name='unique_task_title')
+        ]
+
     def __str__(self):
         return self.title
+#----------------------------------------------------------------------------------------------------------
 
 class Category(models.Model):
     name = models.CharField(max_length=55, unique=True)
 
+    class Meta:
+        db_table = "task_manager_category"
+        verbose_name = "Category"
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='unique_category_name')
+        ]
+
     def __str__(self):
         return self.name
+#--------------------------------------------------------------------------------------------------------------------
 
 class SubTask(models.Model):
-    """Описание: Отдельная часть основной задачи (Task).
-    title: Название подзадачи.
-    description: Описание подзадачи.
-    task: Основная задача. Один ко многим.
-    status: Статус задачи. Выбор из: New, In progress, Pending, Blocked, Done
-    deadline: Дата и время дедлайн.
-    created_at: Дата и время создания. Автоматическое заполнение."""
     title : str = models.CharField(verbose_name="Subtask title", max_length=100 )
     description:str = models.TextField(help_text='описание подзадачи',verbose_name="subtask description", validators=[MinLengthValidator(10), MaxLengthValidator(100)])
     task : Task = models.ForeignKey("Task", on_delete=models.PROTECT, related_name='subtasks', verbose_name="subtask task")
     status : str = models.CharField(max_length=15, choices=STATUS_CHOICES, default='New', verbose_name="subtask status")
     deadline : datetime = models.DateTimeField(help_text="", verbose_name="subtask deadline")
     created_at : datetime = models.DateTimeField(auto_now_add=True, verbose_name="subtask created at")
+
+    class Meta:
+        db_table = "task_manager_subtask"
+        ordering = ['-created_at']
+        verbose_name = "SubTask"
+        constraints = [
+            models.UniqueConstraint(fields=['title'], name='unique_subtask_title')
+        ]
 
     def __str__(self):
         return self.title
